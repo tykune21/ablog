9403879b311341d17ee88a378294fcfe129524c0
tykune21 <tykune21@gmail.com>
2021-09-10 04:22:48 -0500

    ready for heroku hotfix1

--- members/forms.py
+++ members/forms.py
@@ -1,7 +1,7 @@
 from django.contrib.auth.forms import  UserCreationForm, UserChangeForm, PasswordChangeForm
 from django.contrib.auth.models import User
 from django import forms
-from theblog.models import Profile, Category
+from theblog.models import Profile
 
 
 class ProfilePageForm(forms.ModelForm):
