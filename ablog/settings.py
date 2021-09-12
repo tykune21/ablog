c707b855e3b6e3635060923bf8c4f769220982e8
tykune21 <tykune21@gmail.com>
2021-09-12 05:33:43 -0500

    na4

--- ablog/settings.py
+++ ablog/settings.py
@@ -89,12 +89,12 @@ DATABASES = {}
 DATABASES['default'] = dj_database_url.config(conn_max_age=600)
 DATABASES['default']['ENGINE'] = 'django.db.backends.postgresql'
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
-}
+#3DATABASES = {
+ #   'default': {
+  #      'ENGINE': 'django.db.backends.sqlite3',
+   #     'NAME': BASE_DIR / 'db.sqlite3',
+    #}
+#}
 
 
 # Password validation
