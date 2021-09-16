95affcf1dc1eb17effc56de90db1308e4d32623e
tykune21 <tykune21@gmail.com>
2021-09-16 07:24:20 -0500

    finallyfixed3

--- theblog/views.py
+++ theblog/views.py
@@ -1,10 +1,11 @@
-from django.shortcuts import render, get_object_or_404
-from django.views.generic import ListView, DetailView, CreateView, UpdateView, DeleteView
-from .models import Category, Post, Comment
-from .forms import PostForm, EditForm, CommentForm
-from django.urls import reverse_lazy, reverse
 from django.http import HttpResponseRedirect
+from django.shortcuts import render, get_object_or_404
+from django.urls import reverse_lazy, reverse
+# from django.template import context
+from django.views.generic import ListView, DetailView, CreateView, UpdateView, DeleteView
 
+from .forms import PostForm, EditForm, CommentForm
+from .models import Category, Post, Comment
 
 
 def LikeView(request, pk):
@@ -32,34 +33,36 @@ class HomeView(ListView):
 		context["cat_menu"] = cat_menu
 		return context
 
+
+
 def CategoryListView(request):
 	cat_menu_list = Category.objects.all()
-	return render(request, 'category_list.html', {'cat_menu_list':cat_menu_list})
+	return render(request, 'category_list.html', {'cat_menu_list': cat_menu_list})
 
 
 def CategoryView(request, cats):
 	category_posts = Post.objects.filter(category=cats.replace('-', ' '))
-	return render(request, 'categories.html', {'cats':cats.title().replace('-', ' '), 'category_posts':category_posts})
+	return render(request, 'categories.html', {'cats':cats.title().replace('-', ' '), 'category_posts': category_posts})
 
 
 class ArticleDetailView(DetailView):
 	model = Post
 	template_name = 'article_detail.html'
 
-def get_context_data(self, *args, **kwargs):
-	cat_menu = Category.objects.all()
-	context = super(ArticleDetailView, self).get_context_data(*args, **kwargs)
-	stuff = get_object_or_404(Post, id=self.kwargs['pk'])
-	total_likes = stuff.total_likes()
+	def get_context_data(self, *args, **kwargs):
+		cat_menu = Category.objects.all()
+		context = super(ArticleDetailView, self).get_context_data(*args, **kwargs)
+		stuff = get_object_or_404(Post, id=self.kwargs['pk'])
+		total_likes = stuff.total_likes()
 	
-	liked = False
-	if stuff.likes.filter(id=self.request.user.id).exists():
-		liked = True
+		liked = False
+		if stuff.likes.filter(id=self.request.user.id).exists():
+			liked = True
 	
-	context["cat_menu"] = cat_menu
-	context["total_likes"] = total_likes
-	context["liked"] = liked
-	return context
+		context["cat_menu"] = cat_menu
+		context["total_likes"] = total_likes
+		context["liked"] = liked
+		return context
 
 
 class AddPostView(CreateView):
