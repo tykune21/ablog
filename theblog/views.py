07ddc1caae5da6f86224db3553f39736e100f195
tykune21 <tykune21@gmail.com>
2021-09-16 16:27:57 -0500

    finallyfixed2

--- theblog/views.py
+++ theblog/views.py
@@ -34,31 +34,30 @@ class HomeView(ListView):
 		return context
 
 
-
 def CategoryListView(request):
 	cat_menu_list = Category.objects.all()
-	return render(request, 'category_list.html', {'cat_menu_list': cat_menu_list})
+	return render(request, 'category_list.html', {'cat_menu_list':cat_menu_list})
 
 
 def CategoryView(request, cats):
 	category_posts = Post.objects.filter(category=cats.replace('-', ' '))
-	return render(request, 'categories.html', {'cats':cats.title().replace('-', ' '), 'category_posts': category_posts})
+	return render(request, 'categories.html', {'cats':cats.title().replace('-', ' '), 'category_posts':category_posts})
 
 
 class ArticleDetailView(DetailView):
 	model = Post
 	template_name = 'article_detail.html'
-
+	
 	def get_context_data(self, *args, **kwargs):
 		cat_menu = Category.objects.all()
 		context = super(ArticleDetailView, self).get_context_data(*args, **kwargs)
 		stuff = get_object_or_404(Post, id=self.kwargs['pk'])
 		total_likes = stuff.total_likes()
-	
+		
 		liked = False
 		if stuff.likes.filter(id=self.request.user.id).exists():
 			liked = True
-	
+		
 		context["cat_menu"] = cat_menu
 		context["total_likes"] = total_likes
 		context["liked"] = liked
@@ -73,12 +72,12 @@ class AddPostView(CreateView):
 	success_url = reverse_lazy('home')
 
 
-
 class AddCommentView(CreateView):
 	model = Comment
 	form_class = CommentForm
 	template_name = 'add_comment.html'
-	#fields = '__all__'
+	
+	# fields = '__all__'
 	# fields = ('title', 'body')
 	
 	def form_valid(self, form):
@@ -87,17 +86,21 @@ class AddCommentView(CreateView):
 	
 	success_url = reverse_lazy('home')
 
+
 class AddCategoryView(CreateView):
 	model = Category
 	template_name = 'add_category.html'
 	fields = '__all__'
+
+
 # fields = ('title', 'body')
 
 class UpdatePostView(UpdateView):
 	model = Post
 	form_class = EditForm
 	template_name = 'update_post.html'
-	#fields = ['title', 'title_tag', 'body']
+# fields = ['title', 'title_tag', 'body']
+
 
 class DeletePostView(DeleteView):
 	model = Post
